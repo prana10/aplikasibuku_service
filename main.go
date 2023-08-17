3ce33b82095b890e5b24c96ea9740687d378faf1
Prana <pranadhika22@gmail.com>
2023-08-17 13:51:28 +0700

    init main.go

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
-	
+	fmt.Println("Hello, World!")
+
+	router := gin.Default()
+	api := router.Group("/api/v1")
+	api.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "Hello, World!",
+		})
+	})
+
+	router.Run(":8080")
 }
